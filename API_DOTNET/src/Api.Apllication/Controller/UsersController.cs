226161e6d055eaa61d0158b2c50ca3b67a157dcc
Joyce Santos <joycectba@hotmail.com>
2024-05-15 22:13:33 -0300

    Implemented the POST and PUT method

--- API_DOTNET/src/Api.Apllication/Controller/UsersController.cs
+++ API_DOTNET/src/Api.Apllication/Controller/UsersController.cs
@@ -1,5 +1,6 @@
 
 using System.Net;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,8 +10,14 @@ namespace Api.Apllication.Controller
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private IUserService _service;
+
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
         [HttpGet]
-        public async Task<ActionResult> GetAll ([FromServices] IUserService _service)
+        public async Task<ActionResult> GetAll ()
         {
             if(!ModelState.IsValid)
             {
@@ -30,7 +37,7 @@ namespace Api.Apllication.Controller
         [HttpGet]
         [Route("{id}", Name = "GetWithId")]
 
-        public async Task<ActionResult> Get([FromServices] IUserService _service, Guid id)
+        public async Task<ActionResult> Get(Guid id)
         {
             if(!ModelState.IsValid)
             {
@@ -49,7 +56,7 @@ namespace Api.Apllication.Controller
 
         [HttpDelete("{id}")]
         
-        public async Task<ActionResult> Delete([FromServices] IUserService _service, Guid id)
+        public async Task<ActionResult> Delete(Guid id)
         {
             if(!ModelState.IsValid)
             {
@@ -63,6 +70,55 @@ namespace Api.Apllication.Controller
             {
                 
                 return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post ([FromServices] IUserService _service, [FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var result = await _service.Post(user);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetWithId", new {id = result.Id})), result);
+                } else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put([FromServices] IUserService _service, [FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var result = await _service.Put(user);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
             }
         }
 
